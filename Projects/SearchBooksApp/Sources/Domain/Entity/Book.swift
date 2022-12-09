c1520d87c0675104c3bf861049c8b8bf51bb6db4
Safari <111dltldnjs@gmail.com>
2022-12-09 11:37:14 +0900

    feat: Book, Books Model Type 구현 #2

---
+++ Projects/SearchBooksApp/Sources/Domain/Entity/Book.swift
@@ -0,0 +1,20 @@
+//
+//  Book.swift
+//  SearchBooksApp
+//
+//  Created by 이시원 on 2022/12/08.
+//  Copyright © 2022 SearchBooks. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+  let title: String
+  let image: String
+  let author: String
+  let discount: String
+  let publisher: String
+  let isbn: String
+  let description: String
+  let pubdate: String
+}
---
+++ Projects/SearchBooksApp/Sources/Domain/Entity/Books.swift
@@ -0,0 +1,14 @@
+//
+//  Books.swift
+//  SearchBooksApp
+//
+//  Created by 이시원 on 2022/12/08.
+//  Copyright © 2022 SearchBooks. All rights reserved.
+//
+
+import Foundation
+
+struct Books {
+  let total: Int
+  let items: [Book]
+}
