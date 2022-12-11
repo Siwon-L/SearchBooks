bdab4a5f2b41b480679adb056157607b0648c9bb
Safari <111dltldnjs@gmail.com>
2022-12-11 12:59:50 +0900

    refactor: async await를 위한 request 파라미터 수정 #3

--- Projects/SearchBooksApp/Sources/Data/Network/Service/NetworkService.swift
+++ Projects/SearchBooksApp/Sources/Data/Network/Service/NetworkService.swift
@@ -12,7 +12,11 @@ import RxSwift
 
 protocol NetworkServicable {
   func request(endpoint: Endpoint) -> Observable<Data>
-  func request(endpoint: Endpoint, callbackQueue: CallbackQueue) -> Observable<Data>
+  func request(
+    endpoint: Endpoint,
+    callbackQueue: CallbackQueue,
+    completion: @escaping (Result<Data, Error>) -> Void
+  )
 }
 
 final class NetworkService: NetworkServicable {
@@ -23,44 +27,52 @@ final class NetworkService: NetworkServicable {
   }
   
   func request(endpoint: Endpoint) -> Observable<Data> {
-    return request(endpoint: endpoint, callbackQueue: .mainCurrentOrAsync)
+    return Single<Data>.create { [weak self] single in
+      self?.request(endpoint: endpoint, callbackQueue: .mainCurrentOrAsync, completion: { result in
+        switch result {
+        case .success(let data):
+          single(.success(data))
+        case .failure(let error):
+          single(.failure(error))
+        }
+      })
+      return Disposables.create()
+    }.asObservable()
   }
   
   func request(
     endpoint: Endpoint,
-    callbackQueue: CallbackQueue
-  ) -> Observable<Data> {
-    return Single<Data>.create { [weak self] single in
-      let callback: (Result<Data, Error>) -> Void = { result in
-        callbackQueue.execute { single(result) }
-      }
-      do {
-        let urlRequest = try endpoint.create()
+    callbackQueue: CallbackQueue,
+    completion: @escaping (Result<Data, Error>) -> Void
+  ) {
+    let callback: (Result<Data, Error>) -> Void = { result in
+      callbackQueue.execute { completion(result) }
+    }
+    do {
+      let urlRequest = try endpoint.create()
+      
+      urlSession.dataTask(with: urlRequest) { data, response, error in
+        guard error == nil, let data = data else {
+          callback(.failure(SearchServiceError.network(reason: .errorIsOccurred(error.debugDescription))))
+          return
+        }
+        guard let response = response as? HTTPURLResponse else {
+          callback(.failure(SearchServiceError.network(reason: .invalidateResponse)))
+          return
+        }
         
-        self?.urlSession.dataTask(with: urlRequest) { data, response, error in
-          guard error == nil, let data = data else {
-            callback(.failure(SearchServiceError.network(reason: .errorIsOccurred(error.debugDescription))))
-            return
-          }
-          guard let response = response as? HTTPURLResponse else {
-            callback(.failure(SearchServiceError.network(reason: .invalidateResponse)))
-            return
-          }
-
-          switch response.statusCode {
-          case 200..<300: callback(.success(data))
-          case 400: callback(.failure(SearchServiceError.network(reason: .badRequest)))
-          case 401: callback(.failure(SearchServiceError.network(reason:.unauthorized)))
-          case 404: callback(.failure(SearchServiceError.network(reason:.notFound)))
-          case 500: callback(.failure(SearchServiceError.network(reason:.internalServerError)))
-          case 503: callback(.failure(SearchServiceError.network(reason:.serviceUnavailable)))
-          default: callback(.failure(SearchServiceError.network(reason:.unknown)))
-          }
-        }.resume()
-      } catch {
-        callbackQueue.execute { single(.failure(error)) }
-      }
-      return Disposables.create()
-    }.asObservable()
+        switch response.statusCode {
+        case 200..<300: callback(.success(data))
+        case 400: callback(.failure(SearchServiceError.network(reason: .badRequest)))
+        case 401: callback(.failure(SearchServiceError.network(reason:.unauthorized)))
+        case 404: callback(.failure(SearchServiceError.network(reason:.notFound)))
+        case 500: callback(.failure(SearchServiceError.network(reason:.internalServerError)))
+        case 503: callback(.failure(SearchServiceError.network(reason:.serviceUnavailable)))
+        default: callback(.failure(SearchServiceError.network(reason:.unknown)))
+        }
+      }.resume()
+    } catch {
+      callbackQueue.execute { completion(.failure(error)) }
+    }
   }
 }
