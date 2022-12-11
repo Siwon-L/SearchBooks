f18639074f9a4561ea1a2debd5020d9ce7b8c601
Safari <111dltldnjs@gmail.com>
2022-12-11 13:10:08 +0900

    tset: test를 위한 StubFavoritesBookStorage 객체 구현 #3

---
+++ Projects/SearchBooksApp/Tests/TestDouble/Stub/StubFavoritesBookStorage.swift
@@ -0,0 +1,32 @@
+//
+//  StubFavoritesBookStorage.swift
+//  SearchBooksApp
+//
+//  Created by 이시원 on 2022/12/10.
+//  Copyright © 2022 SearchBooks. All rights reserved.
+//
+
+import Foundation
+@testable import SearchBooksApp
+
+import RxSwift
+
+final class StubFavoritesBookStorage: FavoritesBookStoragable {
+  var isbns: [String]
+  
+  init(isbns: [String]) {
+    self.isbns = isbns
+  }
+  
+  var getValue: [String] {
+    return isbns
+  }
+  
+  func addValue(_ isbn: String) {
+    isbns.append(isbn)
+  }
+  
+  func removeValue(_ isbn: String) {
+    isbns.removeAll { $0 == isbn }
+  }
+}
