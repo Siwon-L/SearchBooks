50c6d8cdc66aee1036f81881807f7e25150f678b
Safari <111dltldnjs@gmail.com>
2022-12-23 01:16:35 +0900

    feat: SearchCoordinator 구현 #5

---
+++ Projects/SearchBooksApp/Sources/Presentation/Favorites&Search/Search/Coordinator/SearchCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  SearchCoordinator.swift
+//  SearchBooksApp
+//
+//  Created by 이시원 on 2022/12/19.
+//  Copyright © 2022 SearchBooks. All rights reserved.
+//
+
+import UIKit
+
+final class SearchCoordinator: Coordinator {
+  let navigationController: UINavigationController
+  weak var parentCoordinator: Coordinator?
+  var childCoordinators: [Coordinator] = []
+  
+  init(navigationController: UINavigationController) {
+    self.navigationController = navigationController
+  }
+  
+  func start() {
+    let favoritesBookStorage = FavoritesBookStorage()
+    let networkService = NetworkService()
+    let repository = BooksRepository(networkService: networkService, favoritesBookStorage: favoritesBookStorage)
+    let useCase = SearchBookUseCase(repository: repository)
+    let reactor = SearchReactor(useCase: useCase)
+    let searchViewController = SearchViewController(reactor: reactor)
+    navigationController.pushViewController(searchViewController, animated: true)
+  }
+}
