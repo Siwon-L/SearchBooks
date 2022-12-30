aec8eaf40e70cccaaf43fdbbcdb020c3a26a6080
Safari <111dltldnjs@gmail.com>
2022-12-30 22:02:39 +0900

    feat: NSNotification.Name 확장 #6
    viewWillAppear 프로퍼티 추가

---
+++ Projects/SearchBooksApp/Sources/Presentation/Extension/NSNotificationName+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  NSNotificationName+Extension.swift
+//  SearchBooksApp
+//
+//  Created by 이시원 on 2022/12/30.
+//  Copyright © 2022 SearchBooks. All rights reserved.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+  static let viewWillAppear = NSNotification.Name("viewWillAppear")
+}
